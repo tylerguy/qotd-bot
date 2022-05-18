3592506b041089e9fa50f64b236b791a34b2add2
Tyler Denman <tylerdenman200@gmail.com>
2022-05-18 14:18:00 +0100

    add mysql2 package

---
+++ db/db_init.js
@@ -0,0 +1,25 @@
+// ===============================
+// This file creates the database from the option_models in ./option_models
+// See also: https://discordjs.guide/sequelize
+// ===============================
+
+const Sequelize = require('sequelize')
+const path = require("path")
+const fs = require("fs")
+
+
+const sequelize = new Sequelize('***')
+
+// get all models and save it into DB.<file_name>
+const DB = {}
+const normalized_path = path.join(__dirname, "models")
+fs.readdirSync(normalized_path).forEach(function (file_path) {
+    const file = require("./models/" + file_path)
+    const file_name = file_path.substring(0, file_path.lastIndexOf('.'))
+    DB[file_name] = file(sequelize, Sequelize)
+})
+
+module.exports = {
+    DB,
+    sequelize
+}
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
+const Sequelize = require('sequelize');
+
+const {
+  DB
+} = require('./db/db_init');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-	console.log('Ready!');
+	DB.qotd.sync();
+    console.log('Ready!');
 });
 
 // Login to Discord with your client's token
