8eaa4d7fcb2450eaa5c8431c8025487f321b3780
Tyler Denman <tylerdenman200@gmail.com>
2022-05-18 17:26:06 +0100

    remove timer option

--- index.js
+++ index.js
@@ -28,11 +28,10 @@ var channel;
 // Create a new client instance
 const client = new Discord.Client({
     intents: [
-        Discord.Intents.FLAGS.DIRECT_MESSAGES, Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING, Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
-        Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_INTEGRATIONS, Discord.Intents.FLAGS.GUILD_INVITES,
-        Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Discord.Intents.FLAGS.GUILD_MEMBERS, Discord.Intents.FLAGS.GUILD_BANS
+        Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING
     ],
-    partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'],
+    partials: ['MESSAGE', 'CHANNEL', 'USER'],
 })
 
 // When the client is ready, run this code (only once)
