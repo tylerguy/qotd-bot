ce053037458d599116bb7cea0ada2f6546d2fd70
Tyler Denman <tylerdenman200@gmail.com>
2022-05-18 14:34:15 +0100

    add intents (will fix)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Message } = require('discord.js');
+const { measureMemory } = require('vm');
 const { token } = require('./config.json');
 
 const {
@@ -7,13 +8,58 @@ const {
 } = require('./db/db_init');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Discord.Client({
+  intents: [
+    Discord.Intents.FLAGS.DIRECT_MESSAGES, Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING, Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_INTEGRATIONS, Discord.Intents.FLAGS.GUILD_INVITES,
+    Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Discord.Intents.FLAGS.GUILD_MEMBERS, Discord.Intents.FLAGS.GUILD_BANS
+  ],
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'],
+})
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	DB.qotd.sync();
     console.log('Ready!');
 });
+
+client.on('message', async message => {
+    
+if (message.content.startsWith("!submit")) {
+
+    const submitter = message.member.user.username;
+    const args = message.content.slice("!submit").trim().split(' ').slice(1).join(` `);
+
+    const qotd = await DB.qotd.create({
+
+      Question: `${args}`,
+      Submitter: `${submitter}`
+
+    })
+
+    const qotdembed = new MessageEmbed()
+      .setTitle("New Question added")
+      .setColor(`#1ABC9C`)
+      .addFields({
+        "name": "Question",
+        "value": `${qotd.Question}`
+      }, {
+        "name": "Submitter",
+        "value": `${qotd.Submitter}`
+      })
+    message.channel.send({
+      embeds: [qotdembed]
+    })
+  }
+  if (message.content.startsWith("!random")) {
+    DB.qotd.findOne({
+      Question: 'rand()'
+    }).then((question) => {
+      console.log(question.Question)
+    });
+  }
+    
+})
 
 // Login to Discord with your client's token
 client.login(token);
